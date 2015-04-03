975f7ea6c04f93b06c7267fbd502118ce2dcb5a0
Akshay Gupta <akshay@gakshay.com>
2015-04-03 07:27:15 +0530

    removed unique index on email which is blank

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331185630) do
+ActiveRecord::Schema.define(version: 20150403015208) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20150331185630) do
     t.string   "uid"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["twitter"], name: "index_users_on_twitter"
 
 end
