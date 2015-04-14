101e00cf20b7ccb2d01007585e06e6a9ad515507
Sanjeev Mishra <sanjusoftware@gmail.com>
2015-04-14 10:59:40 +0530

    closes #18

--- app/controllers/request_controller.rb
+++ app/controllers/request_controller.rb
@@ -2,25 +2,32 @@ class RequestController < ApplicationController
 
   def create
 
-    @user = User.where(provider: 'twitter', uid: params[:id]).first_or_create do |user|
-      user.name = params[:name]
-      user.image = params[:profile_image_url]
-      user.twitter = params[:screen_name]
-      user.description = params[:description]
-      user.twitter_verified = params[:verified]
-    end
+    @wishlist = Wishlist.first
+    @requests = current_user.requests.where(:wishlist_id => @wishlist.id)
 
-    if @user.persisted?
-      @request = Request.where(:from => current_user.id, :to => @user.id).first_or_initialize
-      if @request.new_record?
-        @request.status = false
-        @request.wishlist = Wishlist.first
-        if @request.save
-          render 'request/create'
-        end
-      else
-        render :js => "alert('#{@user.name} is already added to your list');"
+    if @requests.count < @wishlist.max_count
+      @user = User.where(provider: 'twitter', uid: params[:id]).first_or_create do |user|
+        user.name = params[:name]
+        user.image = params[:profile_image_url]
+        user.twitter = params[:screen_name]
+        user.description = params[:description]
+        user.twitter_verified = params[:verified]
       end
+
+      if @user.persisted?
+        @request = Request.where(:from => current_user.id, :to => @user.id).first_or_initialize
+        if @request.new_record?
+          @request.status = false
+          @request.wishlist = Wishlist.first
+          if @request.save
+            render 'request/create'
+          end
+        else
+          render :js => "alert('#{@user.name} is already added to your list');"
+        end
+      end
+    else
+      render :js => "alert('Cannot add more, the list has reached its max limit.');"
     end
 
   end
