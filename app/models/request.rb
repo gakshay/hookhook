4795cd360466d5f519bc5234d355f4ec49d2d1e8
Akshay Gupta <akshay@gakshay.com>
2015-04-21 20:34:09 +0530

    changed the color of the labels

--- app/models/request.rb
+++ app/models/request.rb
@@ -6,13 +6,13 @@ class Request < ActiveRecord::Base
   acts_as_taggable_on :pitch
 
   Tags =  [
-      "one question please",
-      "would love to learn from your experience",
-      "need your opinion on something urgent",
-      "in a critical situation, need help",
-      "like to invite you for a coffee",
-      "like to invite you for lunch or dinner",
-      "less than 5 minutes of conversation"
+      [ "one question please", "warning"],
+      [ "would love to learn from your experience", "info"],
+      ["need your opinion on something urgent", "success"],
+      [ "in a critical situation need help", "danger"],
+      [ "like to invite you for a coffee", "info"],
+      [ "like to invite you for lunch or dinner", "warning"],
+      [ "less than 5 minutes of conversation", "success"]
   ]
 
 end
