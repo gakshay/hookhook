df4de0d9ddb9df3263c43ccdc376134881a2aadd
Sanjeev Mishra <sanjusoftware@gmail.com>
2015-05-17 12:46:23 +0530

    campaign commit.. changed the design to fit the video in next page

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,10 +1,8 @@
 class Subscriber < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  }
   validates_presence_of :linkedin, if: :founding_user?
-  validates_presence_of :blog, if: :founding_user?
 
   include SubscriberMail
-
 
   private
 
