cc9a164c72d7d7583af502723e174aecb24f5459
Akshay Gupta <akshay@gakshay.com>
2015-05-18 01:00:59 -0400

    removed * sign from non-required fields

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,6 +1,6 @@
 class Subscriber < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  }
-  validates_presence_of :linkedin, if: :founding_user?
+  validates_presence_of :twitter, if: :founding_user?
 
   include SubscriberMail
 
