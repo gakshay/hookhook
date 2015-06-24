26a8996986ae0c0e25ad7f3766484ddbd0d92b61
Akshay Gupta <akshay@gakshay.com>
2015-06-24 11:44:41 +0530

    cleaned eligibility new form

--- app/models/eligibility.rb
+++ app/models/eligibility.rb
@@ -1,4 +1,20 @@
 class Eligibility < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  }
 
+  DESIGNATION = ["High Scool Student",
+                 "Undergrad Student",
+                 "Masters Student",
+                 "PhD Student",
+                 "Professor/Academician",
+                 "Working Professional",
+                 "Retired Person",
+                 "Free Bird",
+                 "Entrepreneur"
+                ]
+
+  EXPLORE = [ "love",
+              "like",
+              "not like"
+            ]
+
 end
--- app/views/eligibilities/index.json.jbuilder
+++ app/views/eligibilities/index.json.jbuilder
@@ -1,4 +1,4 @@
 json.array!(@eligibilities) do |eligibility|
-  json.extract! eligibility, :id, :designation, :explore, :interest, :priority, :meet
+  json.extract! eligibility, :id, :email, :designation, :explore, :interest, :priority, :meet
   json.url eligibility_url(eligibility, format: :json)
 end
