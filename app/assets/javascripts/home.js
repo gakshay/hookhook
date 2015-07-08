9b40403727ead7145abd21fcfb894001f041eae3
Sanjeev Mishra <sanjusoftware@gmail.com>
2015-07-08 13:38:36 +0530

    closes #57

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,23 +1,3 @@
-$(function(){
-    function stripTrailingSlash(str) {
-        if(str.substr(-1) == '/') {
-            return str.substr(0, str.length - 1);
-        }
-        return str;
-    }
-
-    var url = window.location.pathname;
-    var activePage = stripTrailingSlash(url);
-
-    $('.nav li a.nav_menu').each(function(){
-        var currentPage = stripTrailingSlash($(this).attr('href'));
-
-        if (activePage == currentPage) {
-            $(this).parent().addClass('active');
-        }
-    });
-});
-
 $(document).on('page:update', function () {
     //add javascript that needs to be applied to dynamically added elements in this block
 
