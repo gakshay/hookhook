1947e8dcf91691e32f8a5d5ea27eebf34535147a
Sanjeev Mishra <sanjusoftware@gmail.com>
2015-07-11 10:17:53 +0530

    improvised on the notifications display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,13 @@
 module ApplicationHelper
+  def display_time(time)
+    if time >= Time.now - 24.hours
+      "#{distance_of_time_in_words(Time.now, time)} ago"
+    else
+      time.strftime('%d %b  %Y at %I:%M%p')
+    end
+
+  end
+
   def twitter_url(user)
     "https://twitter.com/#{user.twitter}"
   end
