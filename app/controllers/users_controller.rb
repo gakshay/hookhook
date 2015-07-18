d446287fa14c89be30fe03e81f53f8e93e5687f9
Akshay Gupta <akshay@gakshay.com>
2015-07-18 11:57:27 +0530

    forgot to add the files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:finish_signup]
+
+
+  # GET/PATCH /users/:id/finish_signup
+  def finish_signup
+    # authorize! :update, @user
+    if request.patch? && params[:user] #&& params[:user][:email]
+      if @user.update(user_params)
+        sign_in(@user, :bypass => true)
+      else
+        @show_errors = true
+      end
+    end
+  end
+
+
+  private
+  def set_user
+    @user = current_user || User.find(params[:id])
+  end
+
+  def user_params
+    accessible = [ :name, :email ] # extend with your own params
+    accessible << [ :password, :password_confirmation ] unless params[:user][:password].blank?
+    params.require(:user).permit(accessible)
+  end
+end
