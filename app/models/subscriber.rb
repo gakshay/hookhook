07d9d7d466b7971dd2a62c53a10cc0c060b3dfe6
Akshay Gupta <akshay@gakshay.com>
2015-07-27 13:16:00 +0530

    added subscriber name

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,5 +1,6 @@
 class Subscriber < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  }
+  validates :name, presence: true
   validates_presence_of :twitter, if: :founding_user?
 
   include SubscriberMail
