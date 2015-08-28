d44604f480d86cddb408deb40fe9a2596442f222
Akshay Gupta <akshay@gakshay.com>
2015-08-28 10:42:42 +0530

    private message should be hidden along with it's response

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -10,4 +10,9 @@ module RequestsHelper
   def replier(req)
     current_user == req.to_user ? 'you' : req.to_user.name
   end
+
+  def visible?(request)
+    request.public? || current_user == request.to_user || current_user == request.from_user
+  end
+
 end
